3dbe948bbd61cc07eb22532f97bc2ef81f2a57aa
albertoelias123 <albertoelias.amaral@gmail.com>
2023-06-30 21:54:12 -0300

    feat: Implement UserController with CRUD operations
    The UserController class has been implemented to handle CRUD operations for managing users. The following methods have been added:  index: Displays a listing of users based on the user's role. Administrators and moderators can view all users, while regular users can only view users of type "regular".  store: Stores a newly created user in the database. The request data is validated, and if valid, a new user is created.  show: Displays the details of a specific user.  update: Updates the details of a specific user. The request data is validated, and if valid, the user is updated with the new data.  destroy: Removes a specific user from the database.  These changes improve the user management functionality and provide a complete set of operations for managing users.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (Auth::user()->isAdministrator() || Auth::user()->isMod()) {
+            return (User::paginate());
+        }
+
+        if(Auth::user()->isRegular()) {
+            return (User::whereType('regular')->paginate());
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'type' => 'required',
+        ]);
+
+        $user = User::create($data);
+
+        return response([
+            'message' => __('User created successfully'),
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return response([
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'type' => 'required',
+        ]);
+
+        $user->update($data);
+
+        return response([
+            'message' => __('User updated successfully'),
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response([
+            'message' => __('User deleted successfully')
+        ]);
+    }
+}
