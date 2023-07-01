13109b81e70e692801831e0e989508044d47406b
albertoelias123 <albertoelias.amaral@gmail.com>
2023-06-30 21:29:57 -0300

    feat: Add factories and seeders for database initialization

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,6 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'type' => fake()->randomElement(['admin', 'mod', 'regular']),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
@@ -33,6 +34,39 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the model should be an admin.
+     * Updates the 'type' attribute of the model to 'admin'.
+     */
+    public function isAdmin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'type' => 'admin',
+        ]);
+    }
+
+    /**
+     * Indicate that the model should be a moderator.
+     * Updates the 'type' attribute of the model to 'mod'.
+     */
+    public function isMod(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'type' => 'mod',
+        ]);
+    }
+
+    /**
+     * Indicate that the model should be a regular user.
+     * Updates the 'type' attribute of the model to 'regular'.
+     */
+    public function isRegular(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'type' => 'regular',
         ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create(
+            [
+                'name' => 'Test User',
+                'password' => 'password',
+                'email' => 'test@example.com',
+                'email_verified_at' => now(),
+                'type' => 'admin'
+            ]
+        );
+
+        User::factory(5)
+            ->isRegular()
+            ->create();
     }
 }
